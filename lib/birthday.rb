bc7b009b385a8c347c425a40f52dd1c1c9b86599
Matthew McCarthy <mmccarthy64@hotmail.com>
2020-08-24 20:06:48 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -7,9 +7,9 @@
 def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
     if age <= 12
-    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-  else
-end
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    else
+    end
   end
 end
 
